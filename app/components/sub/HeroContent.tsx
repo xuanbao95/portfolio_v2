49906c1071b06a9c90a2e7ad1b87cddbdf658278
bao95 <bao.luugx@upos.vn>
2023-11-24 14:30:57 +0700

    update

--- app/components/sub/HeroContent.tsx
+++ app/components/sub/HeroContent.tsx
@@ -13,7 +13,7 @@ const HeroContent = ()=>{
         >
             <div className={'h-full w-full flex flex-col gap-5 justify-center m-auto text-start'}>
                 <motion.div
-                    variant={slideInFromTop}
+                    variants={slideInFromTop}
                     className={'Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'}
                 >
                     <SparklesIcon className={'text-[#b49bff] mr-[10px] h-5 w-5'} />
@@ -22,7 +22,7 @@ const HeroContent = ()=>{
                     </h1>
                 </motion.div>
                 <motion.div
-                    variant={slideInFromLeft(0.5)}
+                    variants={slideInFromLeft(0.5)}
                     className={'flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'}
                 >
                     Providing
@@ -33,21 +33,21 @@ const HeroContent = ()=>{
                 </motion.div>
 
                 <motion.p
-                    variant={slideInFromLeft(0.8)}
+                    variants={slideInFromLeft(0.8)}
                     className={'text-lg text-gray-400 my-5 max-w-[600]'}
                 >
                     I&apos;m a Frontend Developer with experience in Website.
                     Check out my project and skills
                 </motion.p>
                 <motion.a
-                    variant={slideInFromLeft(0.5)}
+                    variants={slideInFromLeft(0.5)}
                     className={'py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]'}
                 >
                     Learn More!
                 </motion.a>
             </div>
             <motion.div
-                variant={slideInFromRight(0.8)}
+                variants={slideInFromRight(0.8)}
                 className={'w-full h-full flex justify-center items-center'}
              >
                 <Image
