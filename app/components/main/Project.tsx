1f79991310b1a932d8a2365877121ca267f8e0a3
bao95 <bao.luugx@upos.vn>
2023-12-04 08:37:13 +0700

    project

--- app/components/main/Project.tsx
+++ app/components/main/Project.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
 
-const Project = ({ref} : { ref: any })=>{
+const Project = ()=>{
     return(
         <section id={'projects'} className={'flex flex-col items-center justify-center py-20'}>
             <h1 className={'text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'}>
